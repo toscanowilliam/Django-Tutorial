db87adac432f41275f890f10fca5d14c8e04f9bb
william <william_toscano_@hotmail.com>
2018-05-07 21:06:16 -0700

    part 5 completed

--- polls/models.py
+++ polls/models.py
@@ -15,8 +15,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)    
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now    
 
 
 
@@ -27,3 +29,6 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+
